1839ad8031592b0ceed25055d85917b2d9ad5aa3
Andreas Remigius Schmidt <remigius42-github.dcds7@aleeas.com>
2023-03-25 14:20:57 +0100

    chore: add lint-staged
    - Install lint-staged
    - Create Husky pre-commit hook with
      `npx husky add .husky/pre-commit "npx lint-staged"`
    - Add lint-staged configuration
    
    See https://github.com/okonet/lint-staged#installation-and-setup and
    https://typicode.github.io/husky/#/?id=create-a-hook further details.

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,31 @@
+const LINT_STAGED_CONFIG = {
+  "**/*": files =>
+    mapFilesToInvocations(
+      "prettier --no-error-on-unmatched-pattern --ignore-unknown --list-different",
+      files
+    )
+}
+
+/**
+ * Map the given files to chunks of individual command invocations to
+ * prevent issues with too long command lines.
+ *
+ * See for example https://github.com/okonet/lint-staged/issues/147
+ *
+ * @param {string} command the command to be run with the given files
+ * @param {string[]} files the files to be distributed onto command invocations
+ * @returns {string[]} list of commands to be executed
+ */
+function mapFilesToInvocations(command, files) {
+  const CHUNK_SIZE = 10
+  const invocations = []
+
+  for (let i = 0; i < files.length; i += CHUNK_SIZE) {
+    const currentFilesSlice = files.slice(i, i + CHUNK_SIZE)
+    invocations.push(`${command} ${currentFilesSlice.join(" ")}`)
+  }
+
+  return invocations
+}
+
+export default LINT_STAGED_CONFIG
